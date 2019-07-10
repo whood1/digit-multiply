dc9524a6dfaaf0f64501b953776edfbcb5c3969f
Billy Hood <billylhood@gmail.com>
2019-07-10 14:24:00 -0400

    Save
    Saves generated data to csv when the number has over a specified number of iterations before it hits 1 digit

--- digit-multiply.py
+++ digit-multiply.py
@@ -7,6 +7,9 @@ import numpy as np
 import pandas as pd
 
 d = datetime.datetime.today()
+d = str(d)
+d = d.replace(':', '-')
+
 
 print("[1] Enter a number\n[2] Generate a number\n")
 
@@ -53,3 +56,7 @@ print(more_than_four)
 ndArr = np.array(more_than_four)
 print('2d Array: ')
 print(ndArr)
+
+dataframe = pd.DataFrame(data=ndArr)
+print(dataframe)
+dataframe.to_csv(path_or_buf='C:/Users/billy/Documents/GitHub/digit-multiply/data/' + d + '.csv')
